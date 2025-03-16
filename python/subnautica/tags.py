72f5898662b81600ec411bdec74093b4f11f1da5
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-03-16 19:18:57 +0100

    Initial commit

---
+++ python/subnautica/tags.py
@@ -0,0 +1,22 @@
+"""Tags that can be instantiated on their own
+
+Similar to props, see `props.py`
+"""
+
+# NOTE: Add manually when a new tag/mixin is created
+tag_members: dict[str, list[str]] = {
+    "Eatable": ["hunger_value"],
+    "Drinkable": ["thirst_value"],
+}
+
+
+class Eatable:
+    def __init__(self, hunger_value: int | None = None) -> None:
+        if hunger_value is not None:
+            self.hunger_value = hunger_value
+
+
+class Drinkable:
+    def __init__(self, thirst_value: int | None = None) -> None:
+        if thirst_value is not None:
+            self.thirst_value = thirst_value
