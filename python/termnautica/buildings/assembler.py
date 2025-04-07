011a35bdb2785dbb80c484b97e89802f8a8fdd97
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-04-07 18:46:40 +0200

    Added more buildings

---
+++ python/termnautica/buildings/assembler.py
@@ -0,0 +1,41 @@
+import colex
+from charz import Sprite
+
+from ..item import ItemID, Recipe
+from ..props import Interactable
+from ..fabrication import Fabrication
+
+
+class Assembler(Fabrication, Interactable, Sprite):
+    _RECIPES = [
+        Recipe(
+            products={ItemID.STEEL_KNIFE: 1},
+            idgredients={
+                ItemID.STEEL_BAR: 2,
+                ItemID.STRING: 1,
+            },
+        ),
+        Recipe(
+            products={ItemID.IMPROVED_DIVING_MASK: 1},
+            idgredients={
+                ItemID.DIAMOND: 2,
+                ItemID.STRING: 1,
+                ItemID.FABRIC: 1,
+            },
+        ),
+        Recipe(
+            products={ItemID.ADVANCED_SUITE: 1},
+            idgredients={
+                ItemID.CRYSTAL: 3,
+                ItemID.STEEL_BAR: 1,
+                ItemID.FABRIC: 2,
+            },
+        ),
+    ]
+    centered = True
+    color = colex.BROWN
+    texture = [
+        "/¨¨¨\\",
+        "|   v",
+        "|",
+    ]
---
+++ python/termnautica/buildings/fabricator.py
@@ -0,0 +1,44 @@
+import colex
+from charz import Sprite
+
+from ..item import ItemID, Recipe
+from ..props import Interactable
+from ..fabrication import Fabrication
+
+
+class Fabricator(Fabrication, Interactable, Sprite):
+    _REACH = 4
+    _REACH_FRACTION = 1
+    _RECIPES = [
+        Recipe(
+            products={ItemID.FABRIC: 1},
+            idgredients={ItemID.KELP: 1},
+        ),
+        Recipe(
+            products={ItemID.STRING: 2},
+            idgredients={
+                ItemID.KELP: 1,
+                ItemID.FABRIC: 1,
+            },
+        ),
+        Recipe(
+            products={ItemID.SHARP_ROCK: 1},
+            idgredients={
+                ItemID.IRON_ORE: 2,
+            },
+        ),
+        Recipe(
+            products={ItemID.BASIC_DIVING_MASK: 1},
+            idgredients={
+                ItemID.FABRIC: 2,
+                ItemID.STRING: 1,
+            },
+        ),
+    ]
+    centered = True
+    color = colex.MEDIUM_AQUAMARINE
+    texture = [
+        "__..__",
+        ":    :",
+        "\\.__./",
+    ]
